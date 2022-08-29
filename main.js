4597a156428c8a6c207139e42efd10fffe19b9b9
Yuliia Dyka <yuliia.dyka@umww.com.ua>
2022-08-29 16:06:43 +0300

    refactor: add js for progress

--- main.js
+++ main.js
@@ -10,9 +10,15 @@ const nav_items = document.querySelectorAll('.nav__item');
 const btn = document.querySelector('.btn');
 const buttonsDiv = document.querySelector('.buttons');
 const btn__home = document.querySelector('.home__btn');
+const checkboxes = document.querySelectorAll('.checkbox');
+const progressDig = document.querySelector('.progress__dig');
+const progressLine = document.querySelector('.progress');
+
 
 let currentDesc = description.textContent;
 let currentTitle = title.textContent;
+let progressMax = checkboxes.length;
+
  
 // event listeners
 
@@ -28,7 +34,11 @@ title.textContent = "Welcome to Kottans!";
 description.innerHTML = "We are community of developers who love to code and help each other.The course contains basics of front-end development: HTML, CSS, JavaScript, TypeScript and front-end frameworks. We plan to decrease the amount of lectures in favor of collective discussions, team projects & collective work. Nevertheless, doing that on one's own can be challenging when it comes to continuing without support from surrounding people, such situation really beats the motivation, especially when encountering either boring or impossible to do (at first glance) task or illogical aspect of programming language you can't just grasp.";
 btn.style.display = "flex";
 buttonsDiv.style.display = 'none';
-})
+});
+
+checkboxes.forEach(item => {
+    item.addEventListener('click', calcProgress);
+});
 
 
 // functions declaration
@@ -57,4 +67,16 @@ buttonsDiv.style.display = 'flex';
 
 };
 
+function calcProgress(event) {
+    let currentProgress = 0;
+    checkboxes.forEach(item => {
+        if (item.checked) {
+            currentProgress++;
+        } 
+    });
+
+progressDig.textContent = Math.floor((currentProgress / progressMax * 100)) + "%";
+progressLine.style.width = Math.floor((currentProgress / progressMax * 100)) + "%";
+
+}
 
