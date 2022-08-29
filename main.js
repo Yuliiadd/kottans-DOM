c2a36427638aff8dadf45569933bd60ea381b8ac
Yuliia <yuliiadd@gmail.com>
2022-08-29 20:02:53 +0300

    refactor: fixed js func

--- main.js
+++ main.js
@@ -2,8 +2,6 @@
 
 import dataObj from "./base.js";
 
-// оголошення змінних
-
 const title = document.querySelector('.title__chapter');
 const description = document.querySelector('.description');
 const nav_items = document.querySelectorAll('.nav__item');
@@ -14,13 +12,9 @@ const checkboxes = document.querySelectorAll('.checkbox');
 const progressDig = document.querySelector('.progress__dig');
 const progressLine = document.querySelector('.progress');
 
-const section =  document.querySelector('.section');
-
-
 let currentDesc = description.textContent;
 let currentTitle = title.textContent;
 let progressMax = checkboxes.length;
-
  
 // event listeners
 
@@ -32,8 +26,8 @@ btn__home.addEventListener('click', () => {
 nav_items.forEach(item => {
     item.classList.remove('selected');
 }); 
-title.textContent = "Welcome to Kottans!";
-description.innerHTML = "We are community of developers who love to code and help each other.The course contains basics of front-end development: HTML, CSS, JavaScript, TypeScript and front-end frameworks. We plan to decrease the amount of lectures in favor of collective discussions, team projects & collective work. Nevertheless, doing that on one's own can be challenging when it comes to continuing without support from surrounding people, such situation really beats the motivation, especially when encountering either boring or impossible to do (at first glance) task or illogical aspect of programming language you can't just grasp.";
+changePage("We are community of developers who love to code and help each other.The course contains basics of front-end development: HTML, CSS, JavaScript, TypeScript and front-end frameworks. We plan to decrease the amount of lectures in favor of collective discussions, team projects & collective work. Nevertheless, doing that on one's own can be challenging when it comes to continuing without support from surrounding people, such situation really beats the motivation, especially when encountering either boring or impossible to do (at first glance) task or illogical aspect of programming language you can't just grasp.", "Welcome to Kottans!")
+
 btn.style.display = "flex";
 buttonsDiv.style.display = 'none';
 });
@@ -61,19 +55,22 @@ changePage(currentDesc, currentTitle)
 };
 
 function changePage(currentDesc, currentTitle) {
-// title.textContent = currentTitle;
-// description.innerHTML = currentDesc;
-// btn.style.display = "none";
-// buttonsDiv.style.display = 'flex';
+
+const title = document.querySelector('.title__chapter');
+const description = document.querySelector('.description');
 
 let newTitle = document.createElement("h4");
 newTitle.classList.add('title__chapter');
 newTitle.textContent = currentTitle;
-console.log(newTitle);
-
 title.replaceWith(newTitle);
-console.log(title);
 
+let newDesc = document.createElement('div');
+newDesc.classList.add('description');
+newDesc.innerHTML = currentDesc;
+description.replaceWith(newDesc);
+
+btn.style.display = "none";
+buttonsDiv.style.display = 'flex';
 
 };
 
@@ -87,7 +84,7 @@ function calcProgress(event) {
 
 progressDig.textContent = Math.floor((currentProgress / progressMax * 100)) + "%";
 progressLine.style.width = Math.floor((currentProgress / progressMax * 100)) + "%";
-
-
 };
+
+
 
