136393f3d62df528b913799f1d4bf0be3a8f61d5
Yuliia <yuliiadd@gmail.com>
2022-08-30 09:47:30 +0300

    feat: added event delegation

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import dataObj from "./base.js";
 const title = document.querySelector('.title__chapter');
 const description = document.querySelector('.description');
 const nav_items = document.querySelectorAll('.nav__item');
+const nav = document.querySelector('.nav');
 const btn = document.querySelector('.btn');
 const buttonsDiv = document.querySelector('.buttons');
 const btn__home = document.querySelector('.home__btn');
@@ -18,40 +19,40 @@ let progressMax = checkboxes.length;
  
 // event listeners
 
-nav_items.forEach(item => {
-    item.addEventListener('click', getCurrentContent);
-});
-
-btn__home.addEventListener('click', () => {
-nav_items.forEach(item => {
-    item.classList.remove('selected');
-}); 
-changePage("We are community of developers who love to code and help each other.The course contains basics of front-end development: HTML, CSS, JavaScript, TypeScript and front-end frameworks. We plan to decrease the amount of lectures in favor of collective discussions, team projects & collective work. Nevertheless, doing that on one's own can be challenging when it comes to continuing without support from surrounding people, such situation really beats the motivation, especially when encountering either boring or impossible to do (at first glance) task or illogical aspect of programming language you can't just grasp.", "Welcome to Kottans!")
-
-btn.style.display = "flex";
-buttonsDiv.style.display = 'none';
-});
-
-checkboxes.forEach(item => {
-    item.addEventListener('click', calcProgress);
-});
-
+nav.addEventListener("click", getCurrrentContent);
+btn__home.addEventListener('click', goToHomePage);
 
 // functions declaration
 
-function getCurrentContent(e) {
-nav_items.forEach(item => {
-item.classList.remove('selected');
-}); 
+function getCurrrentContent(e) {
+    nav_items.forEach(item => { item.classList.remove('selected')}); 
+    let target = e.target;
+    switch (target.tagName) {
+        case "LI": 
+        target.classList.add('selected');
+        currentTitle = e.target.innerText;
+        console.log(e.target.innerText);
+        currentDesc = dataObj[currentTitle];
+        changePage(currentDesc, currentTitle);
+        break; 
 
-if (e.path.length == 10) {
-    e.target.parentElement.classList.add('selected');    
-} else {
-    e.target.classList.add('selected');  
-}
-currentTitle = e.target.innerText;
-currentDesc = dataObj[currentTitle];
-changePage(currentDesc, currentTitle)
+        case "A":
+        target.parentElement.classList.add('selected'); 
+        currentTitle = e.target.innerText;
+        console.log(e.target.innerText);
+        currentDesc = dataObj[currentTitle];
+        changePage(currentDesc, currentTitle);
+        break; 
+
+        case "INPUT":
+        let currentProgress = 0;  
+        checkboxes.forEach(item => { if (item.checked) { currentProgress++;}});
+        progressDig.textContent = Math.floor((currentProgress / progressMax * 100)) + "%";
+        progressLine.style.width = Math.floor((currentProgress / progressMax * 100)) + "%";  
+        
+        default:
+        return;
+    };
 };
 
 function changePage(currentDesc, currentTitle) {
@@ -74,17 +75,13 @@ buttonsDiv.style.display = 'flex';
 
 };
 
-function calcProgress(event) {
-    let currentProgress = 0;
-    checkboxes.forEach(item => {
-        if (item.checked) {
-            currentProgress++;
-        } 
-    });
-
-progressDig.textContent = Math.floor((currentProgress / progressMax * 100)) + "%";
-progressLine.style.width = Math.floor((currentProgress / progressMax * 100)) + "%";
+function goToHomePage() {
+    nav_items.forEach(item => {
+        item.classList.remove('selected');
+    }); 
+    changePage("We are community of developers who love to code and help each other.The course contains basics of front-end development: HTML, CSS, JavaScript, TypeScript and front-end frameworks. We plan to decrease the amount of lectures in favor of collective discussions, team projects & collective work. Nevertheless, doing that on one's own can be challenging when it comes to continuing without support from surrounding people, such situation really beats the motivation, especially when encountering either boring or impossible to do (at first glance) task or illogical aspect of programming language you can't just grasp.", "Welcome to Kottans!")
+    btn.style.display = "flex";
+    buttonsDiv.style.display = 'none';
 };
-
 
 
